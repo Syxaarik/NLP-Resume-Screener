81e1f0e909fbf405ce4f81361b8429400fb9d1b0
Artem <artemmarkovskiy2009@gmail.com>
2025-10-26 22:18:10 +0300

    added a text cleaning feature

--- app/clean_text.py
+++ app/clean_text.py
@@ -1,8 +1,9 @@
 import re
 
+
 def cleaner_text(text):
-    text = re.sub(r'\d', '', text)
+    text = text.lower()
+    text = re.sub(r"[\d\+\-\(\)]+", ' ', text)  # удаляем номера
+    text = re.sub(r"[^a-zа-яё\s]", ' ', text)  # убираем знаки
+    text = re.sub(r"\s+", ' ', text).strip()
     print(text)
-
-
-cleaner_text(text)
--- app/pars_text.py
+++ app/pars_text.py
@@ -1,38 +1,52 @@
 import os
 import docx
+from app.clean_text import cleaner_text
 from PyPDF2 import PdfReader
 from PyPDF2.errors import PdfReadError
 
-text_path = input("Enter the path:")  # txt = D:\\Python project\\Project\\DataScinse\\Banword\\data\\resumes\\resume_01.txt
-filename, file_extension = os.path.splitext(text_path) # pdf = D:\Python project\Project\DataScinse\Banword\app\test.pdf
-print(file_extension) # docx = D:\Python project\Project\DataScinse\Banword\app\test.docx
+text_path = input(
+    "Enter the path:")  # txt = D:\\Python project\\Project\\DataScinse\\Banword\\data\\resumes\\resume_01.txt
+filename, file_extension = os.path.splitext(
+    text_path)  # pdf = D:\Python project\Project\DataScinse\Banword\app\test.pdf
+print(file_extension)  # docx = D:\Python project\Project\DataScinse\Banword\app\test.docx
+
 
 def extract_pdf_text(path):
     reader = PdfReader(path)
     text = ""
     for page in reader.pages:
         text += page.extract_text() + "\n"
-    print(text)
+    return text
 
 
 def extract_txt_text(path):
     with open(path, 'r', encoding='utf-8') as file:
-        print(file.readlines())
+        return file.read()
 
 
 def extract_docx_text(path):
     doc = docx.Document(path)
-    full_text = []
-    for para in doc.paragraphs:
-        full_text.append(para.text)
-    print('\n'.join(full_text))
+    full_text = [para.text for para in doc.paragraphs]
+    return '\n'.join(full_text)
 
 
 if file_extension == ".pdf":
-    extract_pdf_text(text_path)
+    print("Текст до обработки:")
+    print(extract_pdf_text(text_path))
+    print("Текст после обработки:")
+    cleaner_text(extract_pdf_text(text_path))
+
 elif file_extension == ".txt":
-    extract_txt_text(text_path)
+    print("Текст до обработки:")
+    print(extract_txt_text(text_path))
+    print("Текст после обработки:")
+    cleaner_text(extract_txt_text(text_path))
+
 elif file_extension == ".docx":
-    extract_docx_text(text_path)
+    print("Текст до обработки:")
+    print(extract_docx_text(text_path))
+    print("Текст после обработки:")
+    cleaner_text(extract_docx_text(text_path))
+
 else:
     raise PdfReadError("File extension not supported")
