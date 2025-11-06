79350ac5b7101b6b12d9fb187530bd775f9c8dc5
Artem <artemmarkovskiy2009@gmail.com>
2025-11-06 20:33:05 +0300

    adding an app_model

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import streamlit as st
 from src.pars_text import file_proc
+from sentence_transformers import SentenceTransformer, util
+
+
+def app_model(resumes, vacancies):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    emb_resume = model.encode(resumes, convert_to_tensor=True)
+    emb_vacancy = model.encode(vacancy, convert_to_tensor=True)
+
+    similarity = util.pytorch_cos_sim(emb_resume, emb_vacancy)
+    return f"{similarity.item():.4f}"
 
 
 st.title("Smart Resume Screener")
