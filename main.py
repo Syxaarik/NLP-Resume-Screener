ec37d8789001f14b998f78296e76964c5d4a7cbe
Artem <artemmarkovskiy2009@gmail.com>
2025-11-07 18:25:08 +0300

    The beta test

--- main.py
+++ main.py
@@ -7,18 +7,19 @@ def app_model(resumes, vacancies):
     model = SentenceTransformer('all-MiniLM-L6-v2')
 
     emb_resume = model.encode(resumes, convert_to_tensor=True)
-    emb_vacancy = model.encode(vacancy, convert_to_tensor=True)
+    emb_vacancy = model.encode(vacancies, convert_to_tensor=True)
 
     similarity = util.pytorch_cos_sim(emb_resume, emb_vacancy)
     return f"{similarity.item():.4f}"
 
 
 st.title("Smart Resume Screener")
-resume = st.file_uploader("Резюме:")
-vacancy = st.file_uploader("Описание вакансии:")
+resume = st.text_input("Резюме:")
+vacancy = st.text_input("Описание вакансии:")
 
 print(resume)
-print(vacancy)
-# if st.button("Оценить"):
-#     score = model.compare(resume, vacancy)
-#     st.write("Совпадение:", score)
+
+
+if st.button("Оценить"):
+     score = app_model(resume, vacancy)
+     st.write("Совпадение:", score)
